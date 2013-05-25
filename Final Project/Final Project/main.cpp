f0d8868f55b5dc8157b412b02150c13324655e44
Rschen7754 <rschen7754@gmail.com>
2013-05-25 00:53:04 -0700

    add files to include

--- Final Project/Final Project/main.cpp
+++ Final Project/Final Project/main.cpp
@@ -6,7 +6,35 @@
 //
 //
 
+#ifdef WIN32
+#include <windows.h>
+#include "GL/glew.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+#else
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#ifdef WIN32
+#include "GL/freeglut.h"
+#else
+#include <GLUT/glut.h>
+#endif
 #include <iostream>
+#include "Angel/Angel.h"
+
+#ifdef __APPLE__
+#define glutInitContextVersion(a,b)
+#define glutInitContextProfile(a)
+#define glewExperimental int glewExperimentalAPPLE
+#define glewInit()
+#endif
 
 int main(int argc, const char * argv[])
 {
