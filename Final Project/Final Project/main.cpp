ee5f4b7fa530a1eab08b2e7e546c1f920c324a7d
Rschen7754 <rschen7754@gmail.com>
2013-05-24 22:25:57 -0700

    empty project

---
+++ Final Project/Final Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by rschen7754 on 5/24/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
