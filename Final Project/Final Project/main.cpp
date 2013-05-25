8d7b70634e1e47f02955cfa36add35ecbb1db7ac
Rschen7754 <rschen7754@gmail.com>
2013-05-25 16:30:22 -0700

    more files

--- Final Project/Final Project/main.cpp
+++ Final Project/Final Project/main.cpp
@@ -26,7 +26,8 @@
 #else
 #include <GLUT/glut.h>
 #endif
-#include <iostream>
+
+
 #include "Angel/Angel.h"
 
 #ifdef __APPLE__
@@ -36,15 +37,62 @@
 #define glewInit()
 #endif
 
+int Width = 800;
+int Height = 800 ;
+
+void init() {
+    GLuint program = InitShader( "vshader.glsl", "fshader.glsl" );
+    glUseProgram(program);
+}
+
+void displayHandler() {
+    
+}
+
+void keyHandler(unsigned char key, int x, int y) {
+    
+}
+void reshapeHandler(int w, int h) {
+    
+}
+void buttonHandler(int button, int state, int x, int y) {
+    
+}
+
+void motionHandler(int x, int y) {
+    
+}
 void idleHandler() {
     
 }
 
-int main(int argc, const char * argv[])
+int main(int argc, char** argv)
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+    glutInit(&argc, argv);
+    // If your code fails to run, uncommenting these lines may help.
+    //glutInitContextVersion(3, 2);
+    //glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition (0, 0);
+    glutInitWindowSize(Width,Height);
+    glutCreateWindow( "Texture Cubes");
+    //printf("GL version %s\n", glGetString(GL_VERSION));
+    glewExperimental = GL_TRUE;
+    glewInit();
+    
+    init();
+    
+    glutIdleFunc(idleHandler) ;
+    glutReshapeFunc (reshapeHandler);
+    glutKeyboardFunc( keyHandler );
+    glutMouseFunc(buttonHandler) ;
+    glutMotionFunc(motionHandler) ;
+    //instructions();
+    
+    glutDisplayFunc(displayHandler);
+    glutMainLoop();
+    
+    return 0;         // never reached
 
+}
